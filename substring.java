59f1193959aeda9e4c641a4517b9bd42e2e42e4f
Abhay <chauhan27.abhay@gmail.com>
2020-07-18 14:27:00 +0530

    01 substring

---
+++ substring.java
@@ -0,0 +1,15 @@
+class substring{
+
+    public static void substring(String str){
+        for(int i=0; i<str.length(); i++){
+            for(int j=i+1; j<=str.length(); j++){
+                System.out.println(str.substring(i,j));
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        String str = "abcd";
+        substring(str);
+    }
+}
