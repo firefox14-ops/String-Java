90c4e7078c73acf276cfcda0d30e5cd69fe3725e
Abhay <chauhan27.abhay@gmail.com>
2020-08-13 12:03:44 +0530

    reverse String word by word

---
+++ reverseString.java
@@ -0,0 +1,39 @@
+// reverse string word by word and remove trailing spaces, if any;
+
+class reverseString{
+
+    public static String reverseString(String s){
+        int i= s.length()-1;
+        String ans = "";
+        while(i>=0){
+
+            while(i>=0 && s.charAt(i) == ' '){
+                i--;
+            }
+
+            int j=i;
+
+            if(i<0){
+                break;
+            }
+
+            while(i>=0 && s.charAt(i) != ' '){
+                i--;
+            }
+
+            if(ans.isEmpty()){
+                ans += s.substring(i+1, j+1);
+            }else{
+                ans += " " + s.substring(i+1, j+1);
+            }
+
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args){
+        String s = "  The sky is blue  ";
+        System.out.println(reverseString(s));
+    }
+}
